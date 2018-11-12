b8268f64dad10df03d3add8029426f245a117d90
megan-klaiber <megan.klaiber@outlook.com>
2018-11-12 21:11:08 +0100

    Updated plots and result. Added exercise 03 for cv/ml track.

--- exercise2/code/run_filter.sh
+++ exercise2/code/run_filter.sh
@@ -2,7 +2,7 @@
 
 #filter_sizes = [1,3,5,7]
 
-python3 cnn_mnist.py --learning_rate=0.1 --filter_size=1
-python3 cnn_mnist.py --learning_rate=0.1 --filter_size=3
-python3 cnn_mnist.py --learning_rate=0.1 --filter_size=5
-python3 cnn_mnist.py --learning_rate=0.1 --filter_size=7
+python3 cnn_mnist.py --filter_size=1 --model_name='1'
+python3 cnn_mnist.py --filter_size=3 --model_name='3'
+python3 cnn_mnist.py --filter_size=5 --model_name='5'
+python3 cnn_mnist.py --filter_size=7 --model_name='7'
--- exercise2/code/run_lr.sh
+++ exercise2/code/run_lr.sh
@@ -2,7 +2,7 @@
 
 #learning_rates = [0.1,0.01,0.001,0.0001]
 
-python3 cnn_mnist.py --learning_rate=0.1
-python3 cnn_mnist.py --learning_rate=0.01
-python3 cnn_mnist.py --learning_rate=0.001
-python3 cnn_mnist.py --learning_rate=0.0001
+python3 cnn_mnist.py --learning_rate=0.1 --model_name='0.1'
+python3 cnn_mnist.py --learning_rate=0.01 --model_name='0.01'
+python3 cnn_mnist.py --learning_rate=0.001 --model_name='0.001'
+python3 cnn_mnist.py --learning_rate=0.0001 --model_name='0.0001'
